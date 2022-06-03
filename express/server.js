346f04b6f8bb0e6ac257d1973fb3a9e88502ffdd
Erik <esnetto@gmail.com>
2022-06-03 13:21:40 -0300

    test deploy on netlify

--- express/server.js
+++ express/server.js
@@ -1,95 +1,104 @@
 'use strict';
 const express = require('express');
-const path = require('path');
+const Web3 = require('web3-eth');
+const Utils = require('web3-utils')
+const axios = require('axios');
+const jsonfile = require('jsonfile')
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-var Contract = require('web3-eth-contract');
-Contract.setProvider('https://clo-geth.0xinfra.com/');
+const web3 = new Web3('https://clo-geth.0xinfra.com/');
 
+//SOY-LP Contract
+const getReservesABI = {"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint112","name":"_reserve0","internalType":"uint112"},{"type":"uint112","name":"_reserve1","internalType":"uint112"},{"type":"uint32","name":"_blockTimestampLast","internalType":"uint32"}],"name":"getReserves","inputs":[],"constant":true}
 
-const contract = new Contract(
-    [
-        {
-            "type":"function",
-            "stateMutability":"view",
-            "outputs":
-                [
-                    {
-                        "type":"uint256",
-                        "name":"",
-                        "internalType":"uint256"
-                    }
-                ],
-            "name":"balanceOf",
-            "inputs":
-                [
-                    {
-                        "type":"address",
-                        "name":"account",
-                        "internalType":"address"
-                    }
-                ]
-        },
-        {
-            "type":"function",
-            "stateMutability":"view",
-            "outputs":
-                [
-                    {
-                        "type":"uint256",
-                        "name":"",
-                        "internalType":"uint256"
-                    }
-                ],
-            "name":"totalSupply",
-            "inputs":[]
-        }
-    ], 
-    '0x9FaE2529863bD691B4A7171bDfCf33C7ebB10a65'
-)
+//GlobalVariables
+let priceList = {}
+const coinGeckoApiIDs = {
+    'SOY': 'soy-finance',
+    'WCLO': 'callisto',
+    'CLOE': false,
+    'BUSDT': 'tether',
+    'ccETC': 'ethereum-classic',
+    'ccETH': 'ethereum',
+    'ccBNB': 'binancecoin',
+    'ccCake': 'pancakeswap-token',
+    'ccTWT': 'trust-wallet-token',
+    'ccWSG': 'wall-street-games',
+    'ccREEF': 'reef-finance',
+    'ccBAKE': 'bakerytoken',
+    'ccSHIBA': 'shiba-inu',
+    'ccRACA': 'radio-caca',
+    'ccLINA': 'linear',
+    'ccTONCOIN': false,
+    'ccXMS': 'mars-ecosystem-token',
+    'ccBTT': 'bittorrent',
+    'ccFTM': 'fantom',
+    'ccANTEX': 'antex',
+    'ccZoo': 'zoo-crypto-world',
+    'ccBCOIN': 'bomber-coin',
+    'ccBBT': 'bitbook-token'
+}
 
-router.get('/circulating',  (req, res) => {
-    contract.methods.totalSupply().call((error, totalSupply) => {
-        if(error) console.log(error);
-        contract.methods.balanceOf("0xdEad000000000000000000000000000000000000").call((error, deadWalletBalance) => {
-            if(error) console.log(error);
-            contract.methods.balanceOf("0x67c20e815D9016CfE04e905A409D276BF1f52b67").call((error, treasuryBalance) => {
-                if(error) console.log(error);
-                contract.methods.balanceOf("0xEbBDd505bA4E6CaD0C17ccd5cbd88CBA073Fe934").call((error, idoBalance) => {
-                  if(error) console.log(error);
-                    res.send(((parseInt(totalSupply) - parseInt(deadWalletBalance) - parseInt(treasuryBalance) - parseInt(idoBalance))/10**18).toString())
-                })
-            })
+async function getReserves(LPTokenAddress) {
+    let LPTokenContract = new web3.Contract([getReservesABI], LPTokenAddress)
+    return await LPTokenContract.methods.getReserves().call()
+}
+
+async function listFarmsAPR(farmList) {
+
+    for (let property in coinGeckoApiIDs) {
+        await getPrice(`${property}`);
+    }
+
+    let farmsAPR = {"farmsAPR": []}
+
+    for (let farm in farmList) {
+        let reserves = await getReserves(farmList[farm].lptoken)
+        let liquidityUSD = priceList[farmList[farm].symbol0]*Utils.fromWei(reserves._reserve0,"ether") + priceList[farmList[farm].symbol1]*Utils.fromWei(reserves._reserve1,"ether")
+        let staking_APR = priceList['SOY']*farmList[farm].yearlysoyreward/liquidityUSD*100
+        farmsAPR['farmsAPR'].push({ 
+            'pair': farmList[farm].symbol0+"-"+farmList[farm].symbol1, 
+            'apr': staking_APR
         })
+    }
 
-      })
-});
+    return farmsAPR
+}
 
-router.get('/total',  (req, res) => {
-    contract.methods.totalSupply().call((error, totalSupply) => {
-        if(error) console.log(error);
-        res.send((parseInt(totalSupply)/10**18).toString())
-    })
-});
+async function getPrice(tokenSymbol){
+    if(coinGeckoApiIDs[tokenSymbol]){
+        await axios
+        .get('https://api.coingecko.com/api/v3/coins/'+coinGeckoApiIDs[tokenSymbol])
+        .then(res => {
+          //console.log(`statusCode: ${res.status}`);
+          setPrice(tokenSymbol, res.data.market_data.current_price.usd);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }else{
+        setPrice(tokenSymbol, 0);
+    }
+}
 
-router.get('/burned',  (req, res) => {
-    contract.methods.balanceOf("0xdEad000000000000000000000000000000000000").call((error, deadWalletBalance) => {
-        if(error) console.log(error);
-        res.send((parseInt(deadWalletBalance)/10**18).toString())
-    })
-});
+function setPrice (symbol, price) {
+    priceList[symbol] = price
+}
+
+
+
 
 router.get('/',  (req, res) => {
-  res.send(
-      "<h1>How to use this API</h1>"+
-      "Use <b>/total</b> to get Total Supply of SOY.<br/>"+
-      "Use <b>/circulating</b> to get Circulating Supply of SOY.<br/>"+
-      "Use <b>/burned</b> to get Burned Amount of SOY."
-  )
+    jsonfile.readFile("./farmlist.json", async function (err, obj) {
+        if (err) console.error(err)
+
+        let list = await listFarmsAPR(obj.farmsInfo)
+        res.send(list)
+    })
 });
 
 app.use(bodyParser.json());
